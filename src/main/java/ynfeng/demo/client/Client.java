bbbcb89e7361dcc2aa1d856e044d5516221647b5
fengyannan <yn.feng@8win.com>
2016-02-19 18:47:24 +0800

    简化客户端代码

--- src/main/java/ynfeng/demo/client/Client.java
+++ src/main/java/ynfeng/demo/client/Client.java
@@ -1,41 +1,29 @@
 package ynfeng.demo.client;
 
+import Ice.Application;
+import Ice.Communicator;
 import ynfeng.demo.PrinterPrx;
 import ynfeng.demo.PrinterPrxHelper;
+import ynfeng.demo.server.MyPrinter;
 
 /**
  * Created by mac on 16/2/17.
  */
-public class Client {
-    public static void main(String[] args)
-    {
-        int status = 0;
-        Ice.Communicator ic = null;
-        try {
-            ic = Ice.Util.initialize(args);
-            Ice.ObjectPrx base = ic.stringToProxy("SimplePrinter:default -p 10000");
-            PrinterPrx printer = PrinterPrxHelper.checkedCast(base);
-            if (printer == null)
-                throw new Error("Invalid proxy");
-
-            printer.printString("Hello Ice!");
-        } catch (Ice.LocalException e) {
-            e.printStackTrace();
-            status = 1;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            status = 1;
-        }
-        if (ic != null) {
-            // Clean up
-            //
-            try {
-                ic.destroy();
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-                status = 1;
-            }
-        }
+public class Client extends Application{
+    public static void main(String[] args) {
+        Client client = new Client();
+        int status = client.main("Server", args);
         System.exit(status);
+    }
+
+    @Override
+    public int run(String[] strings) {
+        Ice.ObjectPrx base = communicator().stringToProxy("SimplePrinter:default -p 10000");
+        PrinterPrx printer = PrinterPrxHelper.checkedCast(base);
+        if (printer == null)
+            throw new Error("Invalid proxy");
+
+        printer.printString("Hello Ice!");
+        return 0;
     }
 }
