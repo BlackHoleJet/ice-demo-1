c9052c17fb361e4a12300bc26d49216858449091
fengyannan <yn.feng@8win.com>
2016-02-19 18:47:44 +0800

    简化服务端代码

--- src/main/java/ynfeng/demo/server/Server.java
+++ src/main/java/ynfeng/demo/server/Server.java
@@ -1,37 +1,29 @@
 package ynfeng.demo.server;
 
+import Ice.Application;
+import Ice.Communicator;
+
 /**
  * Created by mac on 16/2/17.
  */
-public class Server {
+public class Server extends Application {
     public static void main(String[] args) {
-        int status = 0;
-        Ice.Communicator ic = null;
-        try {
-            ic = Ice.Util.initialize(args);
-            Ice.ObjectAdapter adapter =
-                    ic.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000");
-            Ice.Object object = new MyPrinter();
-            adapter.add(object, ic.stringToIdentity("SimplePrinter"));
-            adapter.activate();
-            ic.waitForShutdown();
-        } catch (Ice.LocalException e) {
-            e.printStackTrace();
-            status = 1;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            status = 1;
-        }
-        if (ic != null) {
-            // Clean up
-            //
-            try {
-                ic.destroy();
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-                status = 1;
-            }
-        }
+        Server server = new Server();
+        int status = server.main("Server", args);
         System.exit(status);
+    }
+
+    @Override
+    public int run(String[] strings) {
+        Communicator ic = Application.communicator();
+        Ice.ObjectAdapter adapter =
+                ic.createObjectAdapterWithEndpoints("SimplePrinterAdapter", "default -p 10000");
+        Ice.Object object = new MyPrinter();
+        adapter.add(object, ic.stringToIdentity("SimplePrinter"));
+        adapter.activate();
+        ic.waitForShutdown();
+        if (interrupted())
+            System.err.println(appName() + ": terminating");
+        return 0;
     }
 }
